486a79dd3f90063f0c4d088880520073da7d20ac
venu449 <137485589+venura449@users.noreply.github.com>
2026-02-13 00:16:23 +0530

    beforeall to keep the db

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 // Increase timeout for all tests
 jest.setTimeout(30000);
 
+// Connect to database before tests
+beforeAll(async () => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/af_sliit_project_test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000,
+    });
+  }
+});
+
 // Close any open MongoDB connections after all tests
 afterAll(async () => {
   // Close all MongoDB connections

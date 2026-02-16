e170bbb13bb10ee022080320ff71ca5c8ea3aa34
LochanaNK <lochanakariyawasam10@gmail.com>
2026-02-16 10:56:10 +0530

    added protected routes

--- routes/Lochana/needRoutes.js
+++ routes/Lochana/needRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const needController = require('../../controllers/Lochana/needController.js');
+const { protect, authorize } = require('../../middleware/authmiddleware.js');
 
-router.route('/')
-    .get(needController.getAllNeeds)
-    .post(needController.createNeed);
+// router.route('/')
+//     .get(needController.getAllNeeds)
+//     .post(needController.createNeed);
+
+router.get('/getall', needController.getAllNeeds);
+// router.get('/getfiltered', needController.getFilteredNeeds);
+
+router.post('/create' , protect, needController.createNeed);
 
 module.exports = router;

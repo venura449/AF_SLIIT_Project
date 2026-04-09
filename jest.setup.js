25f73210c3cb42d7ee1c89681c9464db90ad6c4a
venu449 <137485589+venura449@users.noreply.github.com>
2026-04-09 21:55:11 +0530

    critical fix

--- jest.setup.js
+++ jest.setup.js
@@ -6,7 +6,9 @@ jest.setTimeout(30000);
 // Connect to database before tests
 beforeAll(async () => {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/af_sliit_project_test', {
+    const testUri = (process.env.MONGO_URI || 'mongodb://localhost:27017/af_sliit_project_test')
+      .replace(/\/([^/?]+)(\?|$)/, '/$1_jest_test$2');
+    await mongoose.connect(testUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 20000,
